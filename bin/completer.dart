45b3e91c83f0fefbff15faef91d1a7bfc1b076bb
roland-pixel <kharis7181@gmail.com>
2024-12-09 16:12:09 +0700

    completer

--- bin/completer.dart
+++ bin/completer.dart
@@ -1,6 +1,23 @@
+import 'dart:async';
+
 void longRunningtask(
     void Function(String) onDone, void Function(Object?) onError) {
   Future.delayed(Duration(seconds: 5))
       .onError((error, StackTrace) => onError(error))
       .then((value) => onDone("task complete"));
+}
+
+Future<String> runlongRunningTask() {
+  Completer<String> completer = Completer<String>();
+  longRunningtask((data) {
+    completer.complete(data);
+  }, (error) {
+    completer.completeError(error!);
+  });
+  return completer.future;
+}
+
+void main() {
+  runlongRunningTask().then((value) => print(value));
+  print("done");
 }
