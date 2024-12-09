af26addc0e63f1396713d8ca94802c27e49a7a9c
roland-pixel <kharis7181@gmail.com>
2024-12-09 12:40:30 +0700

    periodic timer

---
+++ bin/periodictimer.dart
@@ -0,0 +1,11 @@
+import 'dart:async';
+
+void main() {
+  Timer.periodic(Duration(seconds: 1), (timer) {
+    print("Timer ke ${timer.tick}");
+    if (timer.tick >= 5) {
+      timer.cancel();
+    }
+  });
+  print('done');
+}
