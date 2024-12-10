40850ae351afa493e9d05096a759d0e226e64422
roland-pixel <kharis7181@gmail.com>
2024-12-10 12:41:30 +0700

    generator yield

---
+++ bin/generatortield.dart
@@ -0,0 +1,10 @@
+Stream<int> doubleNumber(int number) async* {
+  yield number;
+  yield number;
+}
+
+Stream<int> asyncNumber() async* {
+  for (int i = 0; i < 10; i++) {
+    yield* doubleNumber(i);
+  }
+}
