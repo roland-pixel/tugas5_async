e166c845127bcbf87348a5979fa668f06c64ef48
roland-pixel <kharis7181@gmail.com>
2024-12-10 12:38:51 +0700

    generator async

---
+++ bin/generatorasync.dart
@@ -0,0 +1,12 @@
+Stream<int> asyncNumber() async* {
+  for (int i = 0; i < 10; i++) {
+    yield i;
+  }
+}
+
+void main() {
+  asyncNumber().listen((event) {
+    print(event);
+  });
+  print('done');
+}
